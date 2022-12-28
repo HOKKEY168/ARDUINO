956b67cbc75337bd4582ad94819c1113bf160083
HOKKEY168 <115516804+HOKKEY168@users.noreply.github.com>
2022-12-28 16:22:15 +0700

    Add files via upload

---
+++ 7Segnmal.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  for ( int x=2; x<=8;x++){
+  pinMode(x, OUTPUT);
+  
+  }
+}
+
+void loop()
+{
+                  
+  digitalWrite(2,1);  //a
+  digitalWrite(3,0);  //b
+  digitalWrite(4,1);  //c
+  digitalWrite(5,1);  //d
+  digitalWrite(6,1);  //e
+  digitalWrite(7,1);  //f
+  digitalWrite(8,1);  //g
+ 
+}
